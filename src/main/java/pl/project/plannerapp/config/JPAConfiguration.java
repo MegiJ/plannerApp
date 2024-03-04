186bce4c6ef8af6c7b13addd4e666258616cb353
MegiJ <jaszczakma@gmail.com>
2024-03-04 21:45:33 +0100

    JPAConfiguration modify

--- src/main/java/pl/project/plannerapp/config/JPAConfiguration.java
+++ src/main/java/pl/project/plannerapp/config/JPAConfiguration.java
@@ -1,17 +1,30 @@
 package pl.project.plannerapp.config;
 
+import com.google.common.collect.ImmutableMap;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.cfg.AvailableSettings;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("pl.project.plannerapp.repo")
 @EnableTransactionManagement
 public class JPAConfiguration {
     @Bean
-    public DataSourceBuilder getDataSourceBuilder() {
+    public DataSource getDataSourceBuilder() {
         DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
         dataSourceBuilder.url("jdbc:mysql://localhost:3306/plannerapp?useUnicode=true&characterEncoding=utf8&" +
@@ -21,4 +34,39 @@ public class JPAConfiguration {
         return dataSourceBuilder.build();
     }
 
+    @Bean
+    public HibernateJpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        return adapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSourceBuilder dataSourceBuilder, HibernateJpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSourceBuilder.build());
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactoryBean.setPackagesToScan("pl.project.plannerapp.model");
+        entityManagerFactoryBean.setJpaPropertyMap(ImmutableMap.of(
+                AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect",
+//                AvailableSettings.SHOW_SQL, "true",
+                AvailableSettings.HBM2DDL_AUTO, "create"
+        ));
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager platformTransactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
