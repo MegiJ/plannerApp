338206b7e70670b19ac4d15d497caf8dc3f48a3d
MegiJ <jaszczakma@gmail.com>
2024-03-25 22:17:26 +0100

    Modify class

--- src/main/java/pl/project/plannerapp/model/ToDo.java
+++ src/main/java/pl/project/plannerapp/model/ToDo.java
@@ -4,6 +4,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.Instant;
+
 public class ToDo {
     @Id
     @GeneratedValue
@@ -11,4 +13,45 @@ public class ToDo {
 
     @NotNull
     private String note;
+
+    @NotNull
+    private Instant date;
+
+    private PersonalData personalData;
+
+    public int getId() {
+        return id;
+    }
+
+    public ToDo setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public ToDo setNote(String note) {
+        this.note = note;
+        return this;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public ToDo setDate(Instant date) {
+        this.date = date;
+        return this;
+    }
+
+    public PersonalData getPersonalData() {
+        return personalData;
+    }
+
+    public ToDo setPersonalData(PersonalData personalData) {
+        this.personalData = personalData;
+        return this;
+    }
 }
