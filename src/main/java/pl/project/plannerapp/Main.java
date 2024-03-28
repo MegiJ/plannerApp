83560fe74f762d94f1d1204d68898a5d5ca65d44
MegiJ <jaszczakma@gmail.com>
2024-03-28 23:09:13 +0100

    Modify class

--- src/main/java/pl/project/plannerapp/Main.java
+++ src/main/java/pl/project/plannerapp/Main.java
@@ -2,6 +2,6 @@ package pl.project.plannerapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
