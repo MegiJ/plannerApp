6bb735c4a251fa7188cc2ef2d27973fb09583b0e
MegiJ <jaszczakma@gmail.com>
2024-04-01 18:09:19 +0200

    Modify Training class

--- src/main/java/pl/project/plannerapp/model/Training.java
+++ src/main/java/pl/project/plannerapp/model/Training.java
@@ -2,11 +2,22 @@ package pl.project.plannerapp.model;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.Instant;
 
 public class Training {
 
     @Id
     @GeneratedValue
     private int t_id;
+
+    @NotNull
+    private Instant date;
+
+    @NotNull
+    private String exercise;
+
+    private PersonalData personalData;
 
 }
