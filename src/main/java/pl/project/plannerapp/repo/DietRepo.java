69dfdcd7eb39cce30dc332ad7282b43c6f424b22
MegiJ <jaszczakma@gmail.com>
2024-04-05 22:51:35 +0200

    Add DietRepo.java

---
+++ src/main/java/pl/project/plannerapp/repo/DietRepo.java
@@ -0,0 +1,10 @@
+package pl.project.plannerapp.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.project.plannerapp.model.Diet;
+
+import java.util.Optional;
+
+public interface DietRepo extends JpaRepository<Diet, Integer> {
+    Optional<Diet> findBy(int id);
+}
