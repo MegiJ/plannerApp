445652bf9261c352d2eab8c8d1a71d61f44d969b
MegiJ <jaszczakma@gmail.com>
2024-04-08 16:20:22 +0200

    Remove Action class and ActionType class Add dockerfile

--- src/main/java/pl/project/plannerapp/DTO/ActionTypeDTO.java
+++
@@ -1,8 +0,0 @@
-package pl.project.plannerapp.DTO;
-
-import net.karneim.pojobuilder.GeneratePojoBuilder;
-
-@GeneratePojoBuilder
-public enum ActionTypeDTO {
-    CALENDAR, OBLIGATION, DIET, TRAINING, SHOPPING_LIST;
-}
--- src/main/java/pl/project/plannerapp/model/ActionType.java
+++
@@ -1,8 +0,0 @@
-package pl.project.plannerapp.model;
-
-import net.karneim.pojobuilder.GeneratePojoBuilder;
-
-@GeneratePojoBuilder
-public enum ActionType {
-    CALENDAR, TODO, DIET, TRAINING, SHOPPING_LIST;
-}
