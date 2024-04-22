28f8da764f35b9af63b38319d772380fa260f0b8
MegiJ <jaszczakma@gmail.com>
2024-04-22 19:30:21 +0200

    Delete ActionRepo.java Modify Dockerfile Modify Main

--- src/main/java/pl/project/plannerapp/Main.java
+++ src/main/java/pl/project/plannerapp/Main.java
@@ -1,5 +1,8 @@
 package pl.project.plannerapp;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
