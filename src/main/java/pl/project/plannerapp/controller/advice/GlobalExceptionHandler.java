04cf3f5562f0f3d9f084810eb85fe47449f33d83
MegiJ <jaszczakma@gmail.com>
2024-05-03 22:03:01 +0200

    Modify GlobalExceptionHandler

--- src/main/java/pl/project/plannerapp/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/pl/project/plannerapp/controller/advice/GlobalExceptionHandler.java
@@ -1,4 +1,37 @@
 package pl.project.plannerapp.controller.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<String> handleSQLIntegrityConstraintViolationException() {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("Cannot delete object. because of existing database connection");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleConstraintViolationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("Invalid data");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethonArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Validation error: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error: " + ex.getReason());
+    }
 }
