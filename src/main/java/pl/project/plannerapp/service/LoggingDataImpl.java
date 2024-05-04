4318c792bb13cb1462fa9802dd28aa3a987ba057
MegiJ <jaszczakma@gmail.com>
2024-05-04 23:01:14 +0200

    modify LoggingDataImpl

--- src/main/java/pl/project/plannerapp/service/LoggingDataImpl.java
+++ src/main/java/pl/project/plannerapp/service/LoggingDataImpl.java
@@ -2,10 +2,15 @@ package pl.project.plannerapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.project.plannerapp.DTO.LogginDataDTO;
 import pl.project.plannerapp.repo.AccountDetailsRepo;
 import pl.project.plannerapp.repo.DietRepo;
 import pl.project.plannerapp.repo.LoggingDataRepo;
 import pl.project.plannerapp.repo.PersonalDataRepo;
+import pl.project.plannerapp.utils.ConventerUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LoggingDataImpl implements LoggingDataService {
@@ -18,5 +23,12 @@ public class LoggingDataImpl implements LoggingDataService {
         this.loggingDataRepo = loggingDataRepo;
         this.personalDataRepo = personalDataRepo;
         this.accountDetailsRepo = accountDetailsRepo;
+    }
+
+    @Override
+    public List<LogginDataDTO> getAll() {
+        return loggingDataRepo.findAll().stream()
+                .map(ConventerUtils::convert)
+                .collect(Collectors.toList());
     }
 }
