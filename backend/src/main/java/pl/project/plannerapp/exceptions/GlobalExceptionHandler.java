f9108ce6ce635d1c9add62f01ed62d38127f179f
MegiJ <jaszczakma@gmail.com>
2024-06-24 08:40:57 +0200

    Delete advice and copy methods

--- backend/src/main/java/pl/project/plannerapp/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/pl/project/plannerapp/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package pl.project.plannerapp.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.server.ResponseStatusException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
@@ -13,6 +16,30 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleIllegalArg(IllegalArgumentException exception, HttpServletRequest request) {
         System.out.println("Blad!");
         return new ResponseEntity<>("Not found", NOT_FOUND);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<String> handleSQLIntegrityConstraintViolationException() {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("Cannot delete object. because of existing database connection");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleConstraintViolationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("Invalid data");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Validation error: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Error: " + ex.getReason());
     }
 
 }
