7a3f2cbe1665e50d7df223217a05d98af5ac4845
MegiJ <jaszczakma@gmail.com>
2024-10-21 16:01:49 +0200

    Add shouldGiveAllAccounts()

--- backend/src/test/java/pl/project/plannerapp/service/AccountDetailsServiceImplTestMock.java
+++ backend/src/test/java/pl/project/plannerapp/service/AccountDetailsServiceImplTestMock.java
@@ -9,6 +9,11 @@ import pl.project.plannerapp.domain.AccountDetailsEntity;
 import pl.project.plannerapp.model.AccountDetails;
 import pl.project.plannerapp.repo.AccountDetailsRepo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -35,5 +40,30 @@ public class AccountDetailsServiceImplTestMock {
         Long result = adsi.addAccount(accountDetails);
         //then
         Assertions.assertEquals(accountEntityDetailsId, result);
+    }
+
+    @Test
+    public void shouldGetAllAccounts() {
+        //given
+        List<AccountDetailsEntity> accountDetailsEntities = new ArrayList<>();
+        accountDetailsEntities.add(AccountDetailsEntity.builder()
+                .id(1L)
+                .role("unicorn")
+                .isExpired(false)
+                .isLocked(false)
+                .isCredentialsExpired(false)
+                .isDisabled(true)
+                .build());
+        when(accountDetailsRepo.findAll()).thenReturn(accountDetailsEntities);
+        //when
+        List<AccountDetails> allAccounts = adsi.getAllAccounts();
+        //then
+        Assertions.assertEquals(1, allAccounts.size());
+        Assertions.assertEquals(1L, allAccounts.get(0).getId());
+        Assertions.assertEquals("unicorn", allAccounts.get(0).getRole());
+        assertFalse(allAccounts.get(0).isExpired());
+        assertFalse(allAccounts.get(0).isLocked());
+        assertFalse(allAccounts.get(0).isCredentialsExpired());
+        assertTrue(allAccounts.get(0).isDisabled());
     }
 }
