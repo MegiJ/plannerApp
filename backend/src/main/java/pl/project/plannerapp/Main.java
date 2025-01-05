fc5050824f2d6e8ec1924f5324f800850e320a4e
MegiJ <jaszczakma@gmail.com>
2025-01-05 21:37:14 +0100

    Ćwiczenia z list

--- backend/src/main/java/pl/project/plannerapp/Main.java
+++ backend/src/main/java/pl/project/plannerapp/Main.java
@@ -1,8 +1,9 @@
 package pl.project.plannerapp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.*;
 
 @ComponentScan(
         basePackages = {
@@ -12,6 +13,19 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        //SpringApplication.run(Main.class, args);
+        Set<String> namesSet = new HashSet<>();
+        namesSet.add("Magda");
+        List<Integer> integerList = new LinkedList<>();
+        integerList.add(10);
+        // System.out.println(integerList.get(0));
+        Queue<Integer> normalQueue = new LinkedList<>();
+        normalQueue.add(10);
+        normalQueue.add(9);
+        normalQueue.add(8);
+        normalQueue.add(9);
+        normalQueue.add(13);
+        System.out.println(normalQueue);
+        System.out.println(normalQueue.peek());
     }
 }
