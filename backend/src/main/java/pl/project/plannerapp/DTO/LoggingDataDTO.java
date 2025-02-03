5ab6f9dfa6af1837f275ddbf2b6a7a80ed792d80
MegiJ <jaszczakma@gmail.com>
2025-02-03 15:17:15 +0100

    Add schema annotation to login; delete exercises for queue

--- backend/src/main/java/pl/project/plannerapp/DTO/LoggingDataDTO.java
+++ backend/src/main/java/pl/project/plannerapp/DTO/LoggingDataDTO.java
@@ -3,13 +3,17 @@ package pl.project.plannerapp.DTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoggingDataDTO {
+    @Schema(requiredMode = REQUIRED, name = "login", description = "login for user")
     private String login;
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, name = "password", description = "password for user", pattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$)")
+    @Schema(requiredMode = REQUIRED, name = "password", description = "password for user",
+            pattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$)")
     private String password;
 }
--- backend/src/main/java/pl/project/plannerapp/Main.java
+++ backend/src/main/java/pl/project/plannerapp/Main.java
@@ -1,9 +1,8 @@
 package pl.project.plannerapp;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.*;
 
 @ComponentScan(
         basePackages = {
@@ -13,19 +12,6 @@ import java.util.*;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //SpringApplication.run(Main.class, args);
-        Set<String> namesSet = new HashSet<>();
-        namesSet.add("Magda");
-        List<Integer> integerList = new LinkedList<>();
-        integerList.add(10);
-        // System.out.println(integerList.get(0));
-        Queue<Integer> normalQueue = new LinkedList<>();
-        normalQueue.add(10);
-        normalQueue.add(9);
-        normalQueue.add(8);
-        normalQueue.add(9);
-        normalQueue.add(13);
-        System.out.println(normalQueue);
-        System.out.println(normalQueue.peek());
+        SpringApplication.run(Main.class, args);
     }
 }
